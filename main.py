ddb8144fc9c93c5ad3ae8d9c64c00e685ac22a53
Santiago <santiago.sanchez.ssaa@gmail.com>
2025-01-08 09:03:34 -0500

    2nd try of make a good db connection for supabase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
@@ -9,7 +9,15 @@ import os
 load_dotenv()
 
 app = FastAPI()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_USER = os.getenv("POSTGRES_USER")
+DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DATABASE_HOST = os.getenv("POSTGRES_HOST")
+DATABASE_PORT = 6543
+DATABASE_NAME = os.getenv("POSTGRES_DATABASE")
+
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+print(f"Database URL: {DATABASE_URL}")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -17,9 +25,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 async def health_check():
     try:
         db = SessionLocal()
-        db.execute("SELECT 1")
+        db.execute(text("SELECT 1"))
         return {"status": "healthy"}
-    except SQLAlchemyError:
+    except SQLAlchemyError as e:
+        print(e)
         return {"status": "unhealthy"}
     finally:
         db.close()
